71f96aa5db45c75eed9f0143b1494e14f1aa58c3
Tran Hoan <tranngocviethoan@gmail.com>
2023-04-21 23:22:13 +0700

    home bottomnav

--- app/src/main/java/com/example/tastyfoods/MainActivity.java
+++ app/src/main/java/com/example/tastyfoods/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.tastyfoods;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.tastyfoods.mvvm.viewmodels.Orders.delivery.DeliveryFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView=(BottomNavigationView) findViewById(R.id.bottomnav);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.homefragment:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,new DeliveryFragment()).addToBackStack(null).commit();
+                        bottomNavigationView.getMenu().findItem(R.id.homefragment).setChecked(true);
+                        break;
+                    case R.id.cartfragment:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,new DeliveryFragment()).addToBackStack(null).commit();
+                        bottomNavigationView.getMenu().findItem(R.id.cartfragment).setChecked(true);
+                        break;
+                    case R.id.deliveryfragment:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,new DeliveryFragment()).addToBackStack(null).commit();
+                        bottomNavigationView.getMenu().findItem(R.id.deliveryfragment).setChecked(true);
+                        break;
+                    case R.id.profilesfragment:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,new DeliveryFragment()).addToBackStack(null).commit();
+                        bottomNavigationView.getMenu().findItem(R.id.profilesfragment).setChecked(true);
+                        break;
+                }
+                return false;
+            }
+        });
+
     }
 }
