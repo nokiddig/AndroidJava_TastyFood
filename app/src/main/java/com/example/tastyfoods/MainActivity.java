9267063a1ecc3919e12292157d0ef306da9edb7e
vu <https://github.com/nokiddig/TastyFood>
2023-04-25 14:30:00 +0700

    Fragments of several lists can be applied

--- app/src/main/java/com/example/tastyfoods/MainActivity.java
+++ app/src/main/java/com/example/tastyfoods/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.tastyfoods;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+
+import com.example.tastyfoods.mvvm.viewmodels.find.FragmentListSearch;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        replaceFragment(new FragmentListSearch());
     }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_layout_listSearch, fragment);
+        fragmentTransaction.commit();
+    }
+
 }
