057743bc5503c21d5093591766a5836e3fb5a3b5
nokiddig <sy2000dn0@gmail.com>
2023-05-06 00:34:22 +0700

    onClick category

--- app/src/main/java/com/example/tastyfoods/mvvm/viewmodels/search/SearchViewModel.java
+++ app/src/main/java/com/example/tastyfoods/mvvm/viewmodels/search/SearchViewModel.java
@@ -4,8 +4,10 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.tastyfoods.mvvm.model.Food;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,6 +35,24 @@ public class SearchViewModel extends ViewModel {
                 e.printStackTrace();
             }
         });
+        return mFoods;
+    }
+
+    public MutableLiveData<List<Food>> findByCategory(int categoryId) {
+        FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+        firebaseFirestore.collection("food").whereEqualTo("categoryId", categoryId)
+            .get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                @Override
+                public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                    List<Food> foods = new ArrayList<>();
+                    int sum = 0;
+                    for (QueryDocumentSnapshot doc : queryDocumentSnapshots) {
+                        Food food = doc.toObject(Food.class);
+                        foods.add(food);
+                    }
+                    mFoods.postValue(foods);
+                }
+            });
         return mFoods;
     }
 }
